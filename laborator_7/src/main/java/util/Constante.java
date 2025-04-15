0c502a9bfe436d23e77ea55b1de37f4c80517762
constantin-voinea <constantin.voinea@endava.com>
2025-04-15 16:25:33 +0300

    Update file path

--- laborator_7/src/main/java/util/Constante.java
+++ laborator_7/src/main/java/util/Constante.java
@@ -8,7 +8,7 @@ public class Constante {
     private Constante() {
     }
 
-    public static final String FISIER_CONTURI = "conturi_bancare.csv";
+    public static final String FISIER_CONTURI = "laborator_7\\src\\main\\resources\\conturi_bancare.csv";
     public static final String MSJ_DEPUNERE_SUCCES = "Am efectuat cu succes operatiunea de depunere a sumei de %.2f in contul %s";
     public static final String MSJ_RETRAGERE_SUCCES = "Am efectuat cu succes operatiunea de retragere a sumei de %.2f din contul %s";
     public static final String MSJ_EROARE_EXECUTIE = "Eroare in procesare: %s";
